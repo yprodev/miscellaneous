af153c8a4182c61074a8f211e22e2df12d60b15e
Continuous Learner <yprodanchuk@gmail.com>
2023-08-12 07:49:11 +0300

    Event Emitter: count the number of listeners on the event

---
+++ event-emitter-e13b7e7908e1/05-listeners-count.js
@@ -0,0 +1,32 @@
+const EventEmitter = require('events');
+
+const myEmitter = new EventEmitter();
+
+const callbackOne = () => {
+  console.log('first event fires');
+};
+
+const callbackTwo = () => {
+  console.log('second event fires');
+};
+
+const callbackThree = () => {
+  console.log('third event fires');
+};
+
+myEmitter.on('firstEvent', callbackOne);
+myEmitter.on('firstEvent', callbackTwo);
+myEmitter.on('firstEvent', callbackThree);
+
+myEmitter.emit('firstEvent');
+
+console.log('Listeners registered --->', myEmitter.listenerCount('firstEvent'));
+
+// Remove listener
+myEmitter.off('firstEvent', callbackOne);
+
+// The first event will be skipped
+myEmitter.emit('firstEvent');
+
+console.log('Listeners registered --->', myEmitter.listenerCount('firstEvent'));
+

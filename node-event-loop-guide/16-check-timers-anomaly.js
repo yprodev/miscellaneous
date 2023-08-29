92f275a68ba6def7e6eb57f1ca5f10af2ca64cba
Continuous Learner <yprodanchuk@gmail.com>
2023-08-29 13:13:43 +0300

    Fix the issues with some examples

--- node-event-loop-guide/16-check-timers-anomaly.js
+++ node-event-loop-guide/16-check-timers-anomaly.js
@@ -1,5 +1,5 @@
 const log = (text) => () => console.log(text);
 
 setTimeout(log('1 | timers'), 0);
-setTimeout(log('2 | check (setImmediate)'));
+setImmediate(log('2 | check (setImmediate)'));
 
--- node-event-loop-guide/17-close-queue.js
+++ node-event-loop-guide/17-close-queue.js
@@ -3,5 +3,7 @@ const fs = require('fs');
 const log = (text) => () => console.log(text);
 const readStream = fs.createReadStream(__filename);
 
-readStream.on('close', log('1 | close queue (close event)');
+readStream.close();
+
+readStream.on('close', log('1 | close queue (close event)'));
 
